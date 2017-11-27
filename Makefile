5a7690ced271e57ea2a7dae036eeab99abd68d5b
Aler Denisov <aler.zampillo@gmail.com>
2017-11-27 13:08:32 +0300

    contract calls

--- Makefile
+++ Makefile
@@ -6,21 +6,15 @@ abi:
 
 dev: abi
 	@$(shell pwd)/node_modules/.bin/cross-env \
-		PRIVATE_KEY=1\
-		FROM_ADDRESS=2\
 		WEB3=http://pub-node1.etherscan.io:8545\
-		GAS_LIMIT=4\
-		GAS_PRICE=5\
-		DEBUG=error,warning,info,log \
+		GAS_PRICE=1000000\
+		DEBUG=error,warning,info,log,verb \
 		$(shell pwd)/node_modules/.bin/nodemon $(shell pwd)/app.js $@ --exec $(shell pwd)/node_modules/.bin/babel-node
 
 run: deps abi
 	@$(shell pwd)/node_modules/.bin/cross-env \
-		PRIVATE_KEY=1\
-		FROM_ADDRESS=2\
 		WEB3=http://pub-node1.etherscan.io:8545\
-		GAS_LIMIT=4\
-		GAS_PRICE=5\
+		GAS_PRICE=1000000\
 		DEBUG=error,warning,info,log \
 		$(shell pwd)/node_modules/.bin/babel-node $(shell pwd)/app.js $@
 
--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 import fastify from 'fastify'
 import Web3 from 'web3'
+import utils from 'ethereumjs-util'
+import Transaction from 'ethereumjs-tx'
 
 const web3 = new Web3(new Web3.providers.HttpProvider(process.env.WEB3))
 
+console.log(process.env.PRIVATE_KEY)
+const CONSTANTS = {
+  networkId: 1,
+  from: process.env.FROM,
+  privateKey: new Buffer(process.env.PRIVATE_KEY, 'hex'),
+  gasPrice: parseInt(process.env.GAS_PRICE)
+}
 const server = fastify()
+
+web3.eth.net.getId((err, result) => {
+  CONSTANTS.networkId = err || result
+})
+
+async function makeTransaction(to, value, data, gasLimit, gasPrice) {
+  const nonce = utils.bufferToHex(await web3.eth.getTransactionCount(CONSTANTS.from))
+  const tx = new Transaction({
+    to,
+    value,
+    data,
+    gasLimit,
+    gasPrice,
+    nonce
+  }, CONSTANTS.chainId)
+
+  tx.sign(CONSTANTS.privateKey)
+  const raw = `0x${tx.serialize().toString('hex')}`
+
+  return new Promise((resolve, reject) => {
+    web3.eth.sendSignedTransaction(raw)
+      .on('transactionHash', hash => {
+        resolve(hash)
+      })
+      .catch(reject)
+  })
+}
 
 const opts = {
   schema: {
@@ -19,6 +55,63 @@ const opts = {
   }
 }
 
+server.post('/contract', opts, async (request, reply) => {
+  if (!request.body.contract) {
+    return reply.send({
+      error: 'Internal Server Error',
+      message: `contract name is required`,
+      statusCode: 500
+    })
+  }
+
+  if (!request.body.method) {
+    return reply.send({
+      error: 'Internal Server Error',
+      message: `contract method is required`,
+      statusCode: 500
+    }) 
+  }
+
+  const json = require(`./abi/${request.body.contract}`)
+  const address = request.body.at || json.networks[CONSTANTS.networkId].address
+  const method = request.body.method
+  let args = request.body.args || []
+  const contract = new web3.eth.Contract(json.abi, address)
+
+
+  const contractMethod = contract.methods[method](...args)
+
+  contractMethod.estimateGas({ gas: 5 * 1e6 }, (error, estimateGas) => {
+    if (error) {
+      return reply.send(error)
+    }
+
+    if (contractMethod._method.constant) {
+      contractMethod.call({}, (error, result) => {
+        if (error) {
+          return reply.send(error)
+        }
+        return reply.send({
+            result,
+            statusCode: 200
+        })
+      })
+    } else {
+      const data = contractMethod.encodeABI()
+      makeTransaction(address, 0, data, estimateGas, CONSTANTS.gasPrice)
+        .then(result => reply.send({
+          result,
+          statusCode: 200
+        }))
+        .catch(error => reply.send({
+          error: 'Internal Server Error',
+          message: error,
+          statusCode: 500
+        }))
+    } 
+  })
+})
+
 server.post('/*', opts, async (request, reply) => {
   const commands = request.params['*'].split('/')
   console.log(request.body)
@@ -33,15 +126,22 @@ server.post('/*', opts, async (request, reply) => {
 
   console.log(func)
 
+  if (!func) {
+    return reply.send({
+      'error': 'Internal Server Error',
+      'message': `web3.eth.${commands.join('.')} is not a function`,
+      'statusCode': 500
+    })
+  }
+
   func(...args, (error, result) => {
     if (error) {
       return reply.send(error)
     }
 
-    return reply.send(
-      {
+    return reply.send({
         result,
-        "statusCode": 200
+        'statusCode': 200
     })
   })
 }) 
