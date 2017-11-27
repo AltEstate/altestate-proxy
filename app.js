61a712a59020f0b50cbecaa7ff0133051ada9852
Aler Denisov <aler.zampillo@gmail.com>
2017-11-27 11:51:59 +0300

    simple facade to web3.eth

--- app.js
+++ app.js
@@ -1,2 +1,46 @@
-console.log(process.env.PRIVATE_KEY)
-console.log(process.argv)
+import fastify from 'fastify'
+import Web3 from 'web3'
+
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.WEB3))
+
+const server = fastify()
+
+const opts = {
+  schema: {
+    body: {
+      type: 'object',
+      properties: {
+        contract: { type: 'string' },
+        at: { type: 'string' },
+        method: { type: 'string' },
+        args: { type: 'array' }
+      }
+    }
+  }
+}
+
+server.post('/*', opts, async (request, reply) => {
+  const commands = request.params['*'].split('/')
+  let func = web3.eth
+
+  for (let part of commands) {
+    console.log('get part:', part)
+    func = func[part]
+  }
+
+  console.log(func)
+
+  func((error, result) => {
+    if (error) {
+      return reply.send(error)
+    }
+
+    return reply.send(result)
+  })
+}) 
+
+// Run the server!
+server.listen(3000, function (err) {
+  if (err) throw err
+  server.log.info(`server listening on ${server.server.address().port}`)
+})
